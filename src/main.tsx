d2dd4c931e33a25eec76312a62b205db73ad72c0
Sanskriti86 <goursanskriti28@gmail.com>
2025-05-29 13:40:09 +0530

    Rebuild with updated dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,4 @@ import App from './App.tsx'
 import './index.css'
 import { HashRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
